385f8b400e00904aebf315bf4b6315ffec40cddf
v2538n <v2538n@gmail.com>
2020-08-17 03:05:51 +0300

    header block top-line fixed animate

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,35 @@
-/*document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
+
+	const animateTrigger = document.getElementsByClassName('header');
+	const animateTriggerHeight = animateTrigger[0].offsetHeight;
+
+	const animateTarget  = document.getElementsByClassName('header__top-line');
+	const animateTargetHeight = animateTarget[0].offsetHeight;
+
+
+	window.addEventListener('scroll', animateOnScroll);
+
+	function animateOnScroll(params){
+		
+		if(pageYOffset > animateTriggerHeight){
+			animateTarget[0].classList.remove('animation_backward');
+			animateTrigger[0].classList.add('animation_active');
+		} else if (animateTrigger[0].classList.contains('animation_active')) {
+			animateTrigger[0].classList.remove('animation_active');
+			animateTarget[0].classList.add('animation_backward');
+		}
+		
+	}
+
+	function offset(el){
+	 	const rect = el.getBoundingClientRect(),
+	 		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+	 		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+	 	return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+	 }
+
+	setTimeout(() => {
+	 	animateOnScroll();
+	 }, 300);
 
 });
-*/
