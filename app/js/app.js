82d38d40efa1f6a8f96ae05697b77a3084a606cf
v2538n <v2538n@gmail.com>
2020-08-27 23:35:26 +0300

    add team section without adaptive version

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,73 @@
+(function($){
+	$(document).ready(function(){
+		
+		function burgerMenu(){
+			$('.header__burger').click(function(){
+				$('.header__burger, .header__menu').toggleClass('active');
+				$('body').toggleClass('lock');
+
+			});
+
+			$('.header__menu').click(function(eventObject){
+				$('.header__burger, .header__menu').removeClass('active');
+				$('body').removeClass('lock');
+				
+				/*if(!$(eventObject.target).hasClass('active')){
+					
+				} else {
+					console.log($(eventObject.target));
+				}*/
+			
+			});
+		}
+
+		burgerMenu();
+
+		
+		function scrollAnimation(){
+			const animateTrigger = $('.header');
+			const animateTriggerHeight = animateTrigger.outerHeight(true);
+
+			const animateTarget  = $('.header__top-line');
+			const animateTargetHeight = animateTarget.outerHeight(true);
+
+
+			window.addEventListener('scroll', animateOnScroll);
+
+			function animateOnScroll(params){
+				
+				if(pageYOffset > animateTriggerHeight){
+					animateTarget.removeClass('animation_backward');
+					animateTrigger.addClass('animation_active');
+				} 
+				else if (animateTrigger.hasClass('animation_active')) {
+					animateTrigger.removeClass('animation_active');
+					animateTarget.addClass('animation_backward');
+				}
+			}
+
+			function offset(el){
+			 	const rect = el.getBoundingClientRect(),
+			 		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			 		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			 	return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
+			 }
+
+			setTimeout(() => {
+			 	animateOnScroll();
+			}, 300);
+		}
+
+		scrollAnimation();
+
+
+		$('.header__menu-list').children('a').click(function(){
+			$(this).addClass('active').siblings().removeClass('active');
+		});
+
+	});
+})(jQuery);
+
 /*document.addEventListener("DOMContentLoaded", function() {
 
 	const animateTrigger = document.getElementsByClassName('header');
@@ -34,64 +104,3 @@
 
 });
 */
-
-(function($){
-	$(document).ready(function(){
-		$('.header__burger').click(function(){
-			$('.header__burger, .header__menu').toggleClass('active');
-			$('body').toggleClass('lock');
-
-		});
-
-		$('.header__menu').click(function(eventObject){
-			$('.header__burger, .header__menu').removeClass('active');
-			$('body').removeClass('lock');
-			
-			/*if(!$(eventObject.target).hasClass('active')){
-				
-			} else {
-				console.log($(eventObject.target));
-			}*/
-		
-		});
-
-
-		const animateTrigger = $('.header');
-		const animateTriggerHeight = animateTrigger.outerHeight(true);
-
-		const animateTarget  = $('.header__top-line');
-		const animateTargetHeight = animateTarget.outerHeight(true);
-
-
-		window.addEventListener('scroll', animateOnScroll);
-
-		function animateOnScroll(params){
-			
-			if(pageYOffset > animateTriggerHeight){
-				animateTarget.removeClass('animation_backward');
-				animateTrigger.addClass('animation_active');
-			} 
-			else if (animateTrigger.hasClass('animation_active')) {
-				animateTrigger.removeClass('animation_active');
-				animateTarget.addClass('animation_backward');
-			}
-		}
-
-		function offset(el){
-		 	const rect = el.getBoundingClientRect(),
-		 		scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
-		 		scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-		 	return { top: rect.top + scrollTop, left: rect.left + scrollLeft }
-		 }
-
-		setTimeout(() => {
-		 	animateOnScroll();
-		 }, 300);
-
-
-		$('.header__menu-list').children('a').click(function(){
-			$(this).addClass('active').siblings().removeClass('active');
-		});
-
-	});
-})(jQuery);
